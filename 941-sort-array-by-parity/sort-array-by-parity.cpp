6730c89e286eef7148d96c2a0c291a9a1f9b485a
Syed Mubeen <22oo1csoo1mubeen@gmail.com>
2026-08-01 12:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (99.90%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        int odd_index = 0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]%2 != 0){
+                if(nums[odd_index]%2 != 0) continue;
+                odd_index = i;
+            }
+            else swap(nums[i],nums[odd_index++]);
+        }
+
+        return nums;
+    }
+};
